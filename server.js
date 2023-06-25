a79150d50bbaf73c69ee9a4d4c5a46427b16afbd
Fadi Serapian <fadi.serapian@gmail.com>
2023-06-25 12:51:25 -0400

    Prepare project

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import products from './data/products.js';
 
 dotenv.config();
 
+void connectDB();
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server running...');
-});
-
 app.get('/api/products', (req, res) => {
-    res.json(products);
+    res.send(products);
 });
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product) => product._id === req.params.id);
-
-    res.json(product);
+    const product = products.find((p) => p._id === req.params.id);
+    res.send(product);
 });
+
+// app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.magenta));
